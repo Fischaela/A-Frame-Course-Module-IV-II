b7ced2cc29618efad1e8868e70515b8f55f8f879
Fischaela <michaela.lehr@geildanke.com>
2018-04-13 21:47:54 +0200

    FIX ar pet attribute

--- components/ar-pet.js
+++ components/ar-pet.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent('arpet', {
     petscale: { type: 'vec3' }
   },
   init: function () {
-    this.el.setAttribute('visibility', false);
+    this.el.setAttribute('visible', false);
 
     let model = document.createElement('a-gltf-model');
     model.setAttribute('src', '#' + this.data.modelid);
