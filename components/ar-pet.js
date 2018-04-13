1f31e51bc7a4d4ced2ff4e98b217311311771850
Fischaela <michaela.lehr@geildanke.com>
2018-04-13 22:19:04 +0200

    MOD pet visibility, debugging

--- components/ar-pet.js
+++ components/ar-pet.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent('arpet', {
     petscale: { type: 'vec3', default: '1 1 1' }
   },
   init: function () {
-    this.el.setAttribute('visible', true);
+    this.el.setAttribute('visible', false);
 
     let model = document.createElement('a-gltf-model');
     model.setAttribute('src', '#' + this.data.modelid);
@@ -33,6 +33,7 @@ AFRAME.registerComponent('arpet', {
     let mark = document.querySelector('a-intersection-marker');
 
     raycaster.addEventListener('click', () => {
+      console.log('Cick', mark.getAttribute('position'))
       this.el.setAttribute('position', mark.getAttribute('position'));
       this.el.setAttribute('visible', true);
     });
