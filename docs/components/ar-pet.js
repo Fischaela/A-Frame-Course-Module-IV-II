2fb6f17b7a5625e053856de74ae96b277eb9ad68
Fischaela <michaela.lehr@geildanke.com>
2018-04-14 17:49:27 +0200

    ADD shadow in docs

--- docs/components/ar-pet.js
+++ docs/components/ar-pet.js
@@ -13,14 +13,27 @@ AFRAME.registerComponent('arpet', {
     model.setAttribute('scale', this.data.petscale);
     model.setAttribute('rotation', this.data.petrotation);
     model.setAttribute('position', this.data.petposition);
+    model.setAttribute('shadow', 'receive: false; cast: true;');
 
     this.el.appendChild(model);
 
+    let shadow = document.createElement('a-plane');
+    shadow.setAttribute('width', '2');
+    shadow.setAttribute('height', '2');
+    shadow.setAttribute('position', this.data.position);
+    shadow.setAttribute('rotation', '-90 0 0');
+    shadow.setAttribute('shadow', 'receive: true');
+    shadow.setAttribute('shadow-material', true);
+
+    this.el.appendChild(shadow);
+
     let raycaster = document.querySelector('[ar-raycaster]');
     let mark = document.querySelector('a-intersection-marker a-sphere');
+    let { stringify } = AFRAME.utils.coordinates;
 
     raycaster.addEventListener('click', () => {
-      this.el.setAttribute('position', mark.getAttribute('position'));
+      let position = raycaster.components.cursor.intersection.point;
+      this.el.setAttribute('position', stringify(position));
       this.el.setAttribute('visible', true);
     });
   }
